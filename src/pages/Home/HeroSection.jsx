a41121f64c3fcc29bfe8699f540a6967dca38cfb
Harleens <harleens@mun.ca>
2024-04-14 23:52:11 -0600

    Create HeroSection.jsx

---
+++ src/pages/Home/HeroSection.jsx
@@ -0,0 +1,23 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Harleen</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+          I am a passionate web developer who loves turning creative ideas into functional and visually appealing websites. With a keen eye for design and a strong foundation in HTML, CSS, JavaScript and React. I specialize in creating responsive and user-friendly websites.
+          </p>
+        </div>
+        
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
